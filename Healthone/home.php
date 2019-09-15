67138f563e96a0637901e3616010b430bae43c61
unknown <302837783@student.rocmondriaan.nl>
2019-09-15 14:36:51 +0200

    Eerste demo

---
+++ Healthone/home.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="CSS/homeb.css">
+
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <div class="jumbotron text-center" ">
+    <div class="row">
+        <div class="col-sm-12">
+            <h1 class="text">Zilverenkruis</h1>
+        </div>
+    </div>
+</div>
+<nav class="navbar navbar-expand-sm bg-light navbar-light sticky-top">
+    <button class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="index.php">
+        <img class="navbrand" src="foto/Zilveren-Kruis-logo.png" alt="Logo" >
+    </a>
+    <div class="collapse navbar-collapse" id="collapse_target">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Apotheker</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Doctor</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Verzekeraar</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="over.php">Over ons</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Inloggen</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<div id="wrapper">
+    <main>
+        <h2>Info</h2>
+
+        <div class="article">
+
+            <h2>Geldzaken</h2>
+            <img  id="geldzaken" src="foto/geldzaken.png" alt="foto">
+            <button type="button" class="btn btn-info">Info</button>
+        </div>
+
+
+        <div class="article">
+            <h2>Zwangerschap</h2>
+            <img  id="geldzaken" src="foto/geldzaken.png" alt="foto">
+            <button type="button" class="btn btn-info">Info</button>
+        </div>
+
+        <div class="article">
+
+            <h2>GGZ behandeling online</h2>
+            <img  id="geldzaken" src="foto/geldzaken.png" alt="foto">
+            <button type="button" class="btn btn-info">Info</button>
+        </div>
+
+        <h2>Vragen over uw zorgverzekering?</h2>
+
+        <div class="article">
+            <h2>Krijg ik een online GGZ behandeling vergoed?</h2>
+            <img  id="geldzaken" src="foto/geldzaken.png" alt="foto">
+            <button type="button" class="btn btn-info">Info</button>
+
+
+        </div>
+
+        <div class="article">
+            <h2>Kan ik tussentijds mijn verzekering aanpassen?</h2>
+            <img  id="geldzaken" src="foto/geldzaken.png" alt="foto">
+            <button type="button" class="btn btn-info">Info</button>
+
+
+        </div>
+        <div class="article">
+            <h2>Lagere vergoeding voor mondhygiënist en fluorbehandeling?</h2>
+            <img  id="geldzaken" src="foto/geldzaken.png" alt="foto">
+            <button type="button" class="btn btn-info">Info</button>
+        </div>
+
+<footer id="sticky-footer" class="py-4 bg-secondary text-white-50">
+    <div class="container text-center">
+        <small>Copyright &copy; Zilverenkruis</small>
+    </div>
+</footer>
+</div>
+</body>
+</html>
---
+++ Healthone/over.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="CSS/overb.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <div class="jumbotron text-center" ">
+    <div class="row">
+        <div class="col-sm-12">
+            <h1 class="text">Zilverenkruis</h1>
+        </div>
+    </div>
+</div>
+
+<nav class="navbar navbar-expand-sm bg-light navbar-light sticky-top">
+    <button class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="index.php">
+        <img class="logo" src="foto/Zilveren-Kruis-logo.png" alt="Logo" >
+    </a>
+    <div class="collapse navbar-collapse" id="collapse_target">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Apotheker</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Doctor</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Verzekeraar</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="over.php">Over ons</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Inloggen</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4">
+            <h3>Wat wij doen</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat urna nec lobortis mollis. Donec felis tortor, volutpat at fermentum a, scelerisque sed diam. Vestibulum eu arcu ligula. Proin faucibus magna nec imperdiet aliquam. Vestibulum sagittis ligula nec lorem laoreet, quis placerat risus rhoncus. Integer in nisl sit amet quam ultrices porttitor. Suspendisse semper lobortis augue ut vehicula. Sed sit amet congue urna. Integer a vulputate leo.</p>
+        </div>
+        <div class="col-sm-4">
+            <h3>Wie we zijn</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat urna nec lobortis mollis. Donec felis tortor, volutpat at fermentum a, scelerisque sed diam. Vestibulum eu arcu ligula. Proin faucibus magna nec imperdiet aliquam. Vestibulum sagittis ligula nec lorem laoreet, quis placerat risus rhoncus. Integer in nisl sit amet quam ultrices porttitor. Suspendisse semper lobortis augue ut vehicula. Sed sit amet congue urna. Integer a vulputate leo.</p>
+        </div>
+        <div class="col-sm-4">
+            <h3>Informatie</h3>
+            <p>Telefoon: 06-1111111</p>
+            <p>Email: jsjsjjsj@gmail.com</p>
+            <p>Openingstijden</p>
+            <p>Adres: opstraat</p>
+
+        </div>
+    </div>
+</div>
+<footer id="sticky-footer" class="py-4 bg-secondary text-white-50">
+    <div class="container text-center">
+        <small>Copyright &copy; Zilverenkruis</small>
+    </div>
+</footer>
+</body>
+</html>
